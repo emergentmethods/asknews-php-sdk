f605105392f0d7001f09996bfb10a152602dc24d
kevin <kevindabot@emergentmethods.ai>
2024-11-13 04:30:10 +0000

    release(0.2.20): Release php SDK

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -85,7 +85,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'asknews-sdk-php-0.2.19';
+    protected $userAgent = 'asknews-sdk-php-0.2.20';
 
     /**
      * Debug switch (default set to false)
